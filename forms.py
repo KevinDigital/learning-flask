623ca53fcc410ef12e27b30246552b5c1892cd1c
Kevin Digital <user_name@example.comkl553@cornell.edugit config --global user.name user_nameKevinDigital>
2017-03-03 22:35:48 -0500

    heroku commit

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
-
-class SignupForm(Form):
-    first_name = StringField('First name')
-    last_name = StringField('Last name')
-    email = StringField('Email')
-    password = PasswordField('Password')
-    submit = SubmitField('Sign up')
